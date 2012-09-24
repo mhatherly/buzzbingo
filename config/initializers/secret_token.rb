c0bcafd717d202967ced33546fe6891a3d600296
Mark Hatherly <mwhatherly@alumni.grinnell.edu>
2012-09-23 19:15:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buzzbingo::Application.config.secret_token = '264b1cddb719ab7ca305a58b94ad0f3d0b02d3b27489989db85c61ec9d9c92a26e9701e21a4210da550b8c88bdb8b8c28ee274e8bbab7015fa4cbf0381021435'
