16b9101d7c1903074109a38e9f089c31735f9439
Mark Hatherly <mwhatherly@alumni.grinnell.edu>
2012-10-13 13:34:48 -0500

    fix the failing view test caused by paginate add

--- app/controllers/buzzwords_controller.rb
+++ app/controllers/buzzwords_controller.rb
@@ -3,7 +3,7 @@ class BuzzwordsController < ApplicationController
   # GET /buzzwords
   # GET /buzzwords.json
   def index
-    @buzzwords = Buzzword.all
+    @buzzwords = Buzzword.paginate(page: params[:page], per_page:10)
 
     respond_to do |format|
       format.html # index.html.erb
--- spec/views/buzzwords/index.html.erb_spec.rb
+++ spec/views/buzzwords/index.html.erb_spec.rb
@@ -2,18 +2,34 @@ require 'spec_helper'
 
 describe "buzzwords/index" do
   before(:each) do
-    assign(:buzzwords, [
+  page = 1
+  per_page = 2
+  buzzwords1 = 
+     [
       stub_model(Buzzword,
         :phrase => "Phrase"
       ),
       stub_model(Buzzword,
         :phrase => "Phrase"
       )
-    ])
+    ]
+   total_entries = buzzwords1.count
+   @test = 
+      WillPaginate::Collection.create(page, per_page, total_entries) do |pager|
+           pager.replace(buzzwords1)
+      end
+    assign(:buzzwords,   @test)
+    # for some reason the @est variable is required  to be named in order
+    # for this test to work properly otherwise will_paginate craps out.
+    
+    
   end
+  
+
+  
 
   it "renders a list of buzzwords" do
-    render
+    render 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Phrase".to_s, :count => 2
   end
