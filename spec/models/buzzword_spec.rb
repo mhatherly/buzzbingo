29e227a472511a89420bad05088de91a577b5945
Mark Hatherly <mwhatherly@alumni.grinnell.edu>
2012-10-22 20:16:52 -0500

    write model tests

--- spec/models/buzzword_spec.rb
+++ spec/models/buzzword_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Buzzword do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+  @buzzword = Buzzword.new(phrase: "Lorem Ipsum") 
+  end
+  
+  subject { @buzzword }
+  
+  it { should respond_to(:phrase) }
+  it { should respond_to(:id) }
+  it { should be_valid }
+  describe "when phrase is empty"  do
+   before { @buzzword.phrase = nil }
+   it {should_not be_valid}
+  end
+  describe "when phrase is too long" do
+   before do
+    max_length = 66
+    @buzzword.phrase = "a" * (max_length + 1)
+   end
+   it {should_not be_valid}
+  end
 end
