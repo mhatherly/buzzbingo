7053fe6c0cfa82fa1550aebb6a41cf98988741aa
Mark Hatherly <mwhatherly@alumni.grinnell.edu>
2012-11-04 19:51:45 -0600

    style puzzle page grid

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def phrase_class(phrase)
+    if phrase.upcase == "FREE" then
+       id = "FREE"
+    elsif phrase.length > 40 then
+      id = "long"
+    elsif phrase.length > 20 then
+       id = "med"
+    else
+       id = "short"
+    end
+    return raw('<td class="' + id + '">')
+  end
 end
